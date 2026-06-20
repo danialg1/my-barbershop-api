3898b781afabb3fe9adaec6efbcaa3e06c0696c4
Danial G. <danialgibran0@gmail.com>
2026-06-20 19:35:05 +0700

    Add SMTP user and password definitions

--- config.example.php
+++ config.example.php
@@ -1,3 +1,5 @@
 <?php
 define('MIDTRANS_SERVER_KEY', 'YOUR_MIDTRANS_SERVER_KEY_HERE');
+define('SMTP_USER', 'Email_yang_kamu_pakai');
+define('SMTP_PASSWORD', 'Password_aplikasi_yang_kamu_dapat');
 ?>
